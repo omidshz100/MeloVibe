d19aa5e0eb57c499ecc260f701ebd5312aa2ee24
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2024-04-07 10:48:20 +0200

    Initial Commit

---
+++ MeloVibe/MeloVibeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeloVibeApp.swift
+//  MeloVibe
+//
+//  Created by Omid Shojaeian Zanjani on 07/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MeloVibeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
